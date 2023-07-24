800937641b10628dc23c86c8c5181030b36fe266
Sidrit Trandafili <forge405@gmail.com>
2023-07-24 09:25:20 +0200

    wip
    Signed-off-by: Sidrit Trandafili <forge405@gmail.com>

--- core/config.php
+++ core/config.php
@@ -1,8 +1,8 @@
 <?php
-defined("DBDRIVER")or define('DBDRIVER', 'pgsql');
-defined("DBHOST")or define('DBHOST', getenv('SDI_DB_HOST'));
-defined("DBNAME")or define('DBNAME', getenv('SDI_DB_NAME'));
-defined("DBUSER")or define('DBUSER', getenv('SDI_DB_USER'));
-defined("DBPASS")or define('DBPASS', getenv('SDI_DB_PASS'));
+defined("DBDRIVER")or define('DBDRIVER', 'mysql');
+defined("DBHOST")or define('DBHOST', 'host.docker.internal');
+defined("DBNAME")or define('DBNAME', 'testsdi');
+defined("DBUSER")or define('DBUSER', 'root');
+defined("DBPASS")or define('DBPASS', null);
 define("ROOTMAIN", $_SERVER['DOCUMENT_ROOT'] . "/");
 define("HOSTMAIN", getenv('SDI_HOST_MAIN'));
